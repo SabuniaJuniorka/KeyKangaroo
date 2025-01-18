06a7057c4be7f4d7d5ec1ebbc4cdc7c27d516852
sabunia <sabuniajuniorka@gmail.com>
2025-01-18 23:46:00 +0100

    wrote check length test

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -2,5 +2,5 @@ import pytest
 import source.my_functions as my_functions
 
 def test_generate_password():
-    result = my_functions.generate_password(12)
-    assert len(result) == 12
+    password = my_functions.generate_password(17)
+    assert (len(password) >= 12 and len(password) <= 32)
